de384e848a811a6c7de7f98c9686f3ac99655d46
Daniel Paschal <daniel.paschal@learningfuze.com>
2016-12-20 08:15:13 -0800

    initial commit

---
+++ script.js
@@ -0,0 +1,43 @@
+
+
+
+function queue(){
+	this.data = [];
+	this.add_item = function(){
+
+	}
+	this.remove_item = function(){
+
+	}
+	this.get_length = function(){
+
+	}
+	this.remove_all = function(){
+
+	}
+	this.get_all = function(){
+
+	}
+}
+
+
+/*end queue*/
+
+function stack(){
+	this.data = [];
+	this.add_item = function(){
+
+	}
+	this.remove_item = function(){
+
+	}
+	this.get_length = function(){
+
+	}
+	this.remove_all = function(){
+
+	}
+	this.get_all = function(){
+		
+	}
+}
